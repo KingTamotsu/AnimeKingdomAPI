56a45c5c00226805bbc6a440bf20c875abe01f69
KingTamotsu <kingTamotsu@outlook.com>
2025-10-29 23:26:50 +0100

    Added the command and query for animes

---
+++ src/Application/Animes/Commands/CreateAnime/CreateAnime.cs
@@ -0,0 +1,29 @@
+﻿using AnimeKingdomAPI.Application.Common.Interfaces;
+
+namespace AnimeKingdomAPI.Application.Animes.Commands.CreateAnime;
+
+public record CreateAnimeCommand : IRequest<int>
+{
+}
+
+public class CreateAnimeCommandValidator : AbstractValidator<CreateAnimeCommand>
+{
+    public CreateAnimeCommandValidator()
+    {
+    }
+}
+
+public class CreateAnimeCommandHandler : IRequestHandler<CreateAnimeCommand, int>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreateAnimeCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> Handle(CreateAnimeCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Application/Animes/Queries/GetAnimes/GetAnimes.cs
@@ -0,0 +1,29 @@
+﻿using AnimeKingdomAPI.Application.Common.Interfaces;
+
+namespace AnimeKingdomAPI.Application.Animes.Queries.GetAnimes;
+
+public record GetAnimesQuery : IRequest<AnimesVm>
+{
+}
+
+public class GetAnimesQueryValidator : AbstractValidator<GetAnimesQuery>
+{
+    public GetAnimesQueryValidator()
+    {
+    }
+}
+
+public class GetAnimesQueryHandler : IRequestHandler<GetAnimesQuery, AnimesVm>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetAnimesQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<AnimesVm> Handle(GetAnimesQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
